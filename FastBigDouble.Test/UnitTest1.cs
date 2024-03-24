039168b11dd9d51207cc877fe680f4aff5f26b13
yaho <160444537+yaho-macovill@users.noreply.github.com>
2024-03-24 20:04:50 +0900

    meta

--- FastBigDouble.Test/UnitTest1.cs
+++ FastBigDouble.Test/UnitTest1.cs
@@ -10,26 +10,26 @@ public class Tests
     [Test]
     public void Less()
     {
-        FastBigDouble p0 = new FastBigDouble("1e100000000000000000000000001");
-        FastBigDouble p1 = new FastBigDouble("1e100000000000000000000000002");
+        BigDouble p0 = new BigDouble("1e100000000000000000000000001");
+        BigDouble p1 = new BigDouble("1e100000000000000000000000002");
         Assert.IsTrue(p0 < p1); 
     }
     [Test]
     public void Div()
     {
-        FastBigDouble p0 = new FastBigDouble("1e1000000");
-        FastBigDouble p1 = new FastBigDouble("1e500000");
-        FastBigDouble p2 = new FastBigDouble("1e"+(1000000-500000).ToString());
+        BigDouble p0 = new BigDouble("1e1000000");
+        BigDouble p1 = new BigDouble("1e500000");
+        BigDouble p2 = new BigDouble("1e"+(1000000-500000).ToString());
         Assert.IsTrue((p0 / p1) == p2);
     }
 
     [Test]
     public void PM()
     {
-        FastBigDouble p1 = new FastBigDouble("1e10000");
-        FastBigDouble p2 = new FastBigDouble("1e10000");
-        FastBigDouble p3= new FastBigDouble("1e10000");
-        FastBigDouble p4 = new FastBigDouble("1e10000");
+        BigDouble p1 = new BigDouble("1e10000");
+        BigDouble p2 = new BigDouble("1e10000");
+        BigDouble p3= new BigDouble("1e10000");
+        BigDouble p4 = new BigDouble("1e10000");
 
 
 
@@ -41,18 +41,18 @@ public class Tests
     [Test]
     public void Mul()
     {
-        FastBigDouble res = 1e110;
-        FastBigDouble p1 = 1e100;
-        FastBigDouble p2 = 1e10;
+        BigDouble res = 1e110;
+        BigDouble p1 = 1e100;
+        BigDouble p2 = 1e10;
         Assert.IsTrue((p1 * p2) == res);
     }
     [Test]
     public void TestEquals()
     {
         {
-            FastBigDouble a = new FastBigDouble("1.0A");
-            FastBigDouble b = new FastBigDouble("1000");
-            FastBigDouble c = new FastBigDouble("1e3");
+            BigDouble a = new BigDouble("1.0A");
+            BigDouble b = new BigDouble("1000");
+            BigDouble c = new BigDouble("1e3");
             Assert.IsTrue(a == b && b == c); 
         }  
     } 
